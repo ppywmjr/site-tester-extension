05df79d888997ca675f18481f4d662a7e1529786
Michael J Rust <ppywmjr@gmail.com>
2017-08-27 09:14:17 +0100

    fixed force reload of page and refactoring

--- content.js
+++ content.js
@@ -17,13 +17,16 @@ $( document ).ready(function() {
         if (response.nextPage == "Stop"){
           return null;
         }else{
-        location.assign(response.nextPage);
-        //location.reload();
+          loadNextPage();
         }
       }
     });
 });
 
+function loadNextPage(){
+  location.assign(response.nextPage);
+  location.reload();
+}
 
  function setUpListener(){
         console.log("setUpListener called");
@@ -37,8 +40,7 @@ $( document ).ready(function() {
         if (request.nextPage == "Stop"){
           return null;
         }else{
-        location.assign(request.nextPage);
-        location.reload();
+          loadNextPage();
         }
       });
 }
@@ -51,20 +53,8 @@ function addAllLinksToContentPageLinksArray(){
     var _thisHref = this.href;
     contentPageLinksArray.push(_thisHref);
   });
-//  displayArrayInConsoleLog(contentPageLinksArray);
 }
-/*
-function addURLsToContentPageLinksArray(){
-  console.log("AddURLsToLinksArray called");
-$("a").each(function() {
-  //console.log("looked up next link");
-  var _thisHref = this.href;
-  contentPageLinksArray.push(_thisHref);
-  //console.log(_thisHref);
-  //ifLinkIsUniqueAppendItToArray(_thisHref, contentPageLinksArray);
-});
-}
-*/
+
 function displayArrayInConsoleLog( _thisArray ){
   //console.log("displayArrayInConsoleLog called");
   var thisArrayLength = _thisArray.length;
@@ -91,27 +81,3 @@ function reportErrorToBackground(){
   console.log("window.location.href is: " + currentPageURL);
     chrome.runtime.sendMessage({errorPage: currentPageURL, greeting: "sending error"}, function(response) {});
 }
-
-/*
-function ifLinkIsUniqueAppendItToArray(thisLink, thisLinksArray) {
-console.log("ifLinkIsUniqueAppendItToArray called");
-  var arrayLength = thisLinksArray.length;
-  for (y = 0; y < arrayLength; y++) {
-    if (hrefIsTheSame(thisLink, thisLinksArray[y])) {
-      console.log("duplicate url " + thisLink);
-      return;
-    }
-  }
-  console.log( "unique url");
-  contentPageLinksArray.push(thisLink);
-}
-
-function hrefIsTheSame(link1, link2) {
-console.log("hrefIsTheSame called");
-  if (link1 == link2) {
-    return true;
-  } else {
-    return false;
-  }
-}
-*/

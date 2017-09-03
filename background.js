b9de03706623569adc767dc46bfcfb7fd89e5e93
Michael J Rust <ppywmjr@gmail.com>
2017-09-03 20:17:05 +0100

    refactoring adding test pages adding popup

--- background.js
+++ background.js
@@ -1,76 +1,95 @@
-var isTestRunning = false;
-var linksArray = ["http://www.catherinetaylordance.co.uk/"];
+var isTestNotStartedRunningOrFinished = "notStarted"; //notStarted running or finished
+var linksArray = [];
 var arrayOfPagesWithError = [];
 var numberOfPagesChecked = 0;
-var maxNumberOfPagesToCheck = 100;
-var lookFor = "London";
-var linksMustContain = "catherinetaylordance.co.uk";
+var maxNumberOfPagesToCheck = 10;
+var lookFor = "error";
+var linksMustContain = "testpage";
+var tempLinksArray = [];
 
 chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
-        console.log("Request received from content.");
-        console.log("greeting is " + request.greeting);
-        console.log("error page is " + request.errorPage);
-        //displayArrayInConsoleLog(linksArray);
-        if (request.greeting == "Should I test?"){
-                if (isTestRunning == false){
-                    console.log("response: No");
-                    sendResponse({shouldITest: "No"});
-                } else if (numberOfPagesChecked >= maxNumberOfPagesToCheck){
-                      console.log("maxNumberOfPagesToCheck reached");
-                      isTestRunning = false;
-                      console.log("response: No");
+          console.log("greeting = " + request.greeting);
+          console.log("isTestNotStartedRunningOrFinished is:" + isTestNotStartedRunningOrFinished);
+        switch (request.greeting){
+          case "Should I test?":
+                  tempLinksArray = request.currentPageLinks;
+                  if(shouldTestFinish()){
+                    isTestNotStartedRunningOrFinished = "finished";
+                    console.log("Changing state to finished");
+                  }
+                  switch (isTestNotStartedRunningOrFinished){
+                    case "notStarted":
                       sendResponse({shouldITest: "No"});
-                      if (request.currentPageLinks != null) {
-                        console.log("request.currentPageLinks is not null.")
-                        AddUniqueURLsToLinksArray(request.currentPageLinks);
-                      }
+                      break;
+                    case "finished":
+                      sendResponse({shouldITest: "No"});
                       displayFinalResults();
-                } else {
-                      console.log("response: yes");
-                      if (request.currentPageLinks != null) {
-                          //console.log("ContentpageLinks are: ");
-                          //displayArrayInConsoleLog(request.currentPageLinks);
-                          AddUniqueURLsToLinksArray(request.currentPageLinks);
-                          if (linksArray.length <= numberOfPagesChecked){
-                            sendResponse({shouldITest: "No"});
-                            displayFinalResults();
-                        } else {
-                            var sentNextPage = linksArray[numberOfPagesChecked];
-                        }
+                      break;
+                    case "running":
+                    addUniqueURLsToLinksArray(tempLinksArray);
+                    numberOfPagesChecked = numberOfPagesChecked + 1;
+                    if (numberOfPagesChecked >= linksArray.length){
+                    var sentNextPage = "Stop";
+                    } else {
+                      var sentNextPage = linksArray[numberOfPagesChecked];
+                    }
+                    console.log("Sent next page to check :" + sentNextPage);
+                    sendResponse({shouldITest: "Yes", testFor: lookFor, nextPage: sentNextPage});
+                  }
 
-                          console.log("sentNextPage is: " + sentNextPage);
-                          console.log("number of pages checked is: " + numberOfPagesChecked);
-                        }
-                        sendResponse({shouldITest: "Yes", testFor: lookFor, nextPage: sentNextPage});
-                        numberOfPagesChecked = numberOfPagesChecked + 1;
-                        console.log("numberOfPagesChecked = " + numberOfPagesChecked);
-                }
-        } else if (request.errorPage != null){
-                console.log("Adding to arrayOfPagesWithError:" + request.errorPage);
-                arrayOfPagesWithError.push(request.errorPage);
-                console.log("arrayOfPagesWithError is now:")
-                displayArrayInConsoleLog(arrayOfPagesWithError);
-                if (isTestRunning == false){
-                  displayFinalResults();
-                }
-        }
+            break;
+          case "Sending error":
+            var errorPageToStore = linksArray[numberOfPagesChecked-1];
+            console.log("Adding to arrayOfPagesWithError:" + request.errorPage);
+            arrayOfPagesWithError.push(errorPageToStore);
+            console.log("arrayOfPagesWithError is now:");
+            displayArrayInConsoleLog(arrayOfPagesWithError);
+            sendResponse("Error Stored");
+            if (isTestNotStartedRunningOrFinished == "finished" ){
+              displayFinalResults();
+            }
+          }
+
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     console.log("browser button clicked");
-    isTestRunning = true;
+    isTestNotStartedRunningOrFinished = "running";
+    addUniqueURLsToLinksArray(tempLinksArray);
+    numberOfPagesChecked = numberOfPagesChecked + 1;
     var nextPageToCheck = linksArray[numberOfPagesChecked];
     console.log("Sent next page to check :" + nextPageToCheck);
-    numberOfPagesChecked = numberOfPagesChecked + 1;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {greeting: "StartTesting", testFor: lookFor, nextPage: nextPageToCheck}, function(response) {
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: "StartTesting", testFor: lookFor, nextPage: nextPageToCheck}, function(response) {
       });
     });
 });
 
-function AddUniqueURLsToLinksArray(contentArray){
-  console.log("AddUniqueURLsToLinksArray called");
+function shouldTestFinish(){
+      console.log("shouldTestFinish called");
+    if (isTestNotStartedRunningOrFinished == "notStarted"){
+        console.log("return false");
+        return false;
+    } else if (numberOfPagesChecked >= maxNumberOfPagesToCheck){
+      console.log("return true");
+        return true;
+    } else if (numberOfPagesChecked >= linksArray.length){
+        addUniqueURLsToLinksArray(tempLinksArray);
+        if (numberOfPagesChecked >= linksArray.length){
+          console.log("return true");
+          return true;
+        }
+        console.log("return false");
+        return false;
+    } else {
+      console.log("return false");
+      return false;
+    }
+}
+
+function addUniqueURLsToLinksArray(contentArray){
+  console.log("addUniqueURLsToLinksArray called");
   var arrayLength = contentArray.length;
   //console.log("Content page links array length is " + arrayLength.toString())
   for (x = 0; x < arrayLength; x++) {
@@ -85,20 +104,17 @@ function AddUniqueURLsToLinksArray(contentArray){
 }
 
 function ifLinkIsUniqueAppendItToArray(thisLink, thisLinksArray) {
-//console.log("ifLinkIsUniqueAppendItToArray called");
+  console.log("ifLinkIsUniqueAppendItToArray called");
   var arrayLength = thisLinksArray.length;
   for (y = 0; y < arrayLength; y++) {
     if (hrefIsTheSame(thisLink, thisLinksArray[y])) {
-    //  console.log("duplicate url " + thisLink);
       return;
     }
   }
-  //console.log( "unique url");
   linksArray.push(thisLink);
 }
 
 function hrefIsTheSame(link1, link2) {
-//console.log("hrefIsTheSame called");
   if (link1 == link2) {
     return true;
   } else {
@@ -107,7 +123,7 @@ function hrefIsTheSame(link1, link2) {
 }
 
 function displayArrayInConsoleLog( _thisArray ){
-  //console.log("displayArrayInConsoleLog called");
+  console.log("displayArrayInConsoleLog called");
   var thisArrayLength = _thisArray.length;
   for (z = 0; z < thisArrayLength; z++) {
     console.log( _thisArray[z] );
@@ -115,10 +131,17 @@ function displayArrayInConsoleLog( _thisArray ){
 }
 
 function displayFinalResults(){
+  console.log("displayFinalResults called");
   console.log("The linksArray is:");
   displayArrayInConsoleLog(linksArray);
   console.log("error pages are:");
   displayArrayInConsoleLog(arrayOfPagesWithError);
   console.log("The number of pages tested is " + numberOfPagesChecked.toString())
   console.log("Number of pages with errors is " + arrayOfPagesWithError.length.toString());
+}
+
+function resetExtension(){
+  linksArray = [];
+  arrayOfPagesWithError = [];
+  numberOfPagesChecked = 0;
 }
--- content.js
+++ content.js
@@ -3,7 +3,7 @@ var contentPageLinksArray = [];
 $( document ).ready(function() {
     console.log( "Document ready!" );
     addAllLinksToContentPageLinksArray();
-    console.log("sending message to background");
+    console.log("sending message to background: Should I test?");
     chrome.runtime.sendMessage({greeting: "Should I test?", currentPageLinks: contentPageLinksArray}, function(response) {
       console.log("shouldITest response is: " + response.shouldITest);
       console.log("test for: " + response.testFor);
@@ -15,14 +15,40 @@ $( document ).ready(function() {
       else{
         testPage(response.testFor);
         if (response.nextPage != "Stop"){
-
-          loadNextPage(response.nextPage);
-        }else{
-
+        loadNextPage(response.nextPage);
         }
       }
     });
 });
+
+ function setUpListener(){
+        console.log("setUpListener called");
+        chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+        sendResponse("test started");
+        console.log("extension called from background");
+        console.log("greeting is " + request.greeting);
+        console.log("testFor is " + request.testFor);
+        console.log("nextPage is " + request.nextPage);
+        testPage(request.testFor);
+        if (request.nextPage != null){
+          console.log("next page is not stop");
+          loadNextPage(request.nextPage);
+        }
+      });
+}
+
+function testPage(error){
+  console.log("testPage called")
+  var isError = $( "*:contains('" + error + "')" ).length;
+  console.log("Number of errors on page is " + isError.toString());
+  if (isError > 0){
+      console.log("error found")
+      reportErrorToBackground();
+  }else {
+      console.log("error NOT found")
+  }
+}
 
 function loadNextPage(_nextPage){
   console.log("nextPage is: " + _nextPage);
@@ -32,24 +58,11 @@ function loadNextPage(_nextPage){
   setTimeout(function(){location.reload()}, 1000);
   }
 
- function setUpListener(){
-        console.log("setUpListener called");
-        chrome.runtime.onMessage.addListener(
-        function(request, sender, sendResponse) {
-        console.log("extension called from background");
-        console.log("greeting is " + request.greeting);
-        console.log("testFor is " + request.testFor);
-        console.log("nextPage is " + request.nextPage);
-        testPage(request.testFor);
-        if (request.nextPage == "Stop"){
-          return null;
-        }else{
-          console.log("next page is not stop");
-        //  location.assign(request.nextPage);
-        //  location.reload();
-          loadNextPage(request.nextPage);
-        }
-      });
+function reportErrorToBackground(){
+  var currentPageURL = window.location.href;
+  console.log("window.location.href is: " + currentPageURL);
+  //setTimeout(function(){console.log("waiting")}, 10000);
+  chrome.runtime.sendMessage({errorPage: currentPageURL, greeting: "Sending error"}, function(response){});
 }
 
 function addAllLinksToContentPageLinksArray(){
@@ -68,22 +81,4 @@ function displayArrayInConsoleLog( _thisArray ){
   for (x = 0; x < thisArrayLength; x++) {
     console.log( _thisArray[x] );
   }
-}
-
-function testPage(error){
-  console.log("testPage called")
-  var isError = $( "*:contains('" + error + "')" ).length;
-  console.log("Number of errors on page is " + isError.toString());
-  if (isError > 0){
-      console.log("error found")
-      reportErrorToBackground();
-  }else {
-      console.log("error NOT found")
-  }
-}
-
-function reportErrorToBackground(){
-  var currentPageURL = window.location.href;
-  console.log("window.location.href is: " + currentPageURL);
-    chrome.runtime.sendMessage({errorPage: currentPageURL, greeting: "sending error"}, function(response) {});
 }
